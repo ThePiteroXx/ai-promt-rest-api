befc3cf638cb54cab2c07c5e88f2a74c90d70c68
ThePiteroXx <thepremiumxx@wp.pl>
2023-05-14 23:59:57 +0200

    feat: cache response

--- src/controllers/prompt.ts
+++ src/controllers/prompt.ts
@@ -1,12 +1,29 @@
 import axios from "axios";
 import * as dotenv from "dotenv";
 import { RequestHandler } from "express";
+import Cache from "node-cache";
 
-dotenv.config();
+type ResponseKey =
+  | "id"
+  | "smallImage"
+  | "bigImage"
+  | "prompt"
+  | "prompt_full"
+  | "created_at"
+  | "score"
+  | "time"
+  | "aspect";
+
+type Response = Record<ResponseKey, string>;
 
 type Params = {
   search: string | undefined;
+  page: number | undefined;
 };
+
+const promptCache = new Cache({ stdTTL: 60 * 15 });
+
+dotenv.config();
 
 const headers = {
   "X-RapidAPI-Key": process.env.KEY,
@@ -14,33 +31,59 @@ const headers = {
 };
 
 export const getAllPrompts: RequestHandler = async (req, res, next) => {
-  const data = await axios.get(
-    "https://ai_image-database.p.rapidapi.com/images/midjourney",
-    { headers }
-  );
-  console.log(data.data);
-  return res.status(200).json(data.data);
+  const { page } = req.query as Params;
+  let data: Response[] = [];
+
+  //cache check
+  if (promptCache.has("prompt-list")) {
+    data = promptCache.get("prompt-list") as Response[];
+  } else {
+    const { data: promptData } = await axios.get(
+      "https://ai_image-database.p.rapidapi.com/images/midjourney",
+      { headers }
+    );
+
+    data = promptData;
+    promptCache.set("prompt-list", data);
+  }
+
+  let limit = 20;
+  let paginatedPrompts = [];
+
+  if (page) {
+    paginatedPrompts = data.slice((page - 1) * limit, page * limit);
+  } else {
+    paginatedPrompts = data;
+  }
+
+  return res.status(200).json(paginatedPrompts);
 };
 
 export const getPrompt: RequestHandler = async (req, res, next) => {
   const { search } = req.query as Params;
+  let data: Response[] = [];
+
   if (!search)
     return res
       .status(400)
       .json({ message: "Something gone wrong. Please try later." });
 
-  const response = await axios.get(
-    "https://ai_image-database.p.rapidapi.com/images/midjourney",
-    { headers }
+  // cache check
+  if (promptCache.has("prompt-list")) {
+    data = promptCache.get("prompt-list") as Response[];
+  } else {
+    const { data: promptData } = await axios.get(
+      "https://ai_image-database.p.rapidapi.com/images/midjourney",
+      { headers }
+    );
+
+    data = promptData;
+    promptCache.set("prompt-list", data);
+  }
+
+  const searchedPrompts = data.filter((item: Response) =>
+    item.prompt?.includes(search)
   );
 
-  let limit = 10;
-  const arr: { prompt: string }[] = [];
-
-  response.data.some((item: { prompt: string }) => {
-    if (item.prompt?.includes(search)) arr.push(item);
-    if (arr.length === limit) return true;
-  });
-
-  return res.status(200).json(arr);
+  return res.status(200).json(searchedPrompts);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 import { promptRoute } from "./routes";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/prompt", promptRoute);
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `⚡️[server]: Server is running at http://localhost:${PORT} or http://0.0.0.0:${PORT}`
+  );
 });
